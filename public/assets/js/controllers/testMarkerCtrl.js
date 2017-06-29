1a805dbd368980b2b7bb076fb7429a571c51c410
gperez8 <gregory.facyt@gmail.com>
2017-06-29 09:23:58 -0400

    se corrigieron errores de esLint con respecto a la manipulacion de modelos 3d

--- public/assets/js/controllers/testMarkerCtrl.js
+++ public/assets/js/controllers/testMarkerCtrl.js
@@ -1,4 +1,21 @@
+ /* global $, angular, document */
+
 angular.module('app')
 	.controller('testMarkerCtrl', ($scope) => {
 		$scope.hola = 'hola';
+
+		function handleSceneLoaded() {
+			document.querySelector('#camera').setAttribute('orbit-controls', 'enabled', false);
+			document.querySelector('#camera').setAttribute('position', '0 2 10');
+			document.querySelector('#camera').setAttribute('orbit-controls', 'enabled', true);
+			// ORBIT CAMERA DRAG START / END EVENT LISTENERS
+			document.querySelector('#camera').addEventListener('start-drag-orbit-controls');
+			document.querySelector('#camera').addEventListener('end-drag-orbit-controls');
+		}
+
+		document.addEventListener('DOMContentLoaded', () => {
+			const scene = document.querySelector('a-scene');
+			if (scene.hasLoaded) addEventListeners();
+			else scene.addEventListener('loaded', handleSceneLoaded);
+		});
 	});
