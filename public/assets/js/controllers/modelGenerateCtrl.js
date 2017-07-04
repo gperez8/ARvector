6950855005a5cb6392d4b9bc22315b4fc75be5c0
gperez8 <gregory.facyt@gmail.com>
2017-07-04 16:31:05 -0400

    + se creo plano tridimensional para graficar sobre el

--- public/assets/js/controllers/modelGenerateCtrl.js
+++ public/assets/js/controllers/modelGenerateCtrl.js
@@ -1,176 +1,52 @@
+/* global angular, mathBox, THREE */
 
 angular.module('app')
 	.controller('modelGenerateCtrl', ($scope) => {
-		$scope.hola = "hola mundo";
-		
+		$scope.hola = 'hola mundo';
+		// se crea una instancia de mathBox
+		const mathbox = mathBox({
+			plugins: ['core', 'controls', 'cursor', 'mathbox'],
+			controls: { klass: THREE.OrbitControls },
+		});
+		if (mathbox.fallback) throw 'WebGL not supported';
 
-var mathbox = mathBox({
-      plugins: ['core', 'controls', 'cursor', 'mathbox'],
-      controls: {klass: THREE.OrbitControls}
-    });
-    if (mathbox.fallback) throw "WebGL not supported"
+		// se crea una variable three para acceder a los metodos
+		// three.renderer, three.scene, three.camera
+		const three = mathbox.three;
 
-    var three = mathbox.three;
-    three.renderer.setClearColor(new THREE.Color(0xFFFFFF), 1.0);
+		// limpia el color de la escena y lo coloca en blanco
+		three.renderer.setClearColor(new THREE.Color(0xFFFFFF), 1.0);
 
-	var graphData, view;
-	
-	var functionText = "cos(0.5*x + 0.5*x*y)";
-	
-	var pointText = "(1,1)";
-	
-	var a = 1, b = 1;
-	var	xMin = -3, xMax = 3, yMin = -3,	yMax = 3, zMin = -3, zMax = 3;
-	
-	var zAutofit = true;
-	
-	// start of updateGraph function ==============================================================
-	var updateGraphFunc = function() 
-	{ 
-		var zFunc = Parser.parse( functionText ).toJSFunction( ['x','y'] );
-		graphData.set("expr", 
-		    function (emit, x, y, i, j, t) 
-			{ 
-			    emit( x, zFunc(x,y), y );
-			}
-		);
-		
-		if (zAutofit)
-		{
-			var xStep = (xMax - xMin) / 256;
-			var yStep = (yMax - yMin) / 256;
-			var zSmallest = zFunc(xMin, yMin);
-			var zBiggest  = zFunc(xMin, yMin);
-			for (var x = xMin; x <= xMax; x += xStep)
-			{
-				for (var y = yMin; y <= yMax; y += yStep)
-				{
-					var z = zFunc(x,y);
-					if (z < zSmallest) zSmallest = z;
-					if (z > zBiggest)  zBiggest  = z;
-				}
-			}
-			zMin = zSmallest;
-			zMax = zBiggest;
-		}
-		view.set("range", [[xMin, xMax], [zMin,zMax], [yMin, yMax]]); 
+		three.camera = mathbox.camera({
+			proxy: true,
+			position: [0, 0, 3],
+		});
 
-		if (graphColorStyle == "Grayscale")
-		{
-			// zMax = white, zMin = black
-			graphColors.set("expr", 
-				function (emit, x, y, i, j, t) 
-				{ 
-					var z = zFunc(x,y);
-					var percent = (z - zMin) / (zMax - zMin);
-					emit( percent, percent, percent, 1.0 );
-				}
-			);
-		}
-		else if (graphColorStyle == "Rainbow")
-		{
-			// rainbow hue; zMax = red, zMin = violet			
-			graphColors.set("expr", 
-				function (emit, x, y, i, j, t) 
-				{ 
-					var z = zFunc(x,y);
-					var percent = (z - 1.2 * zMin) / (zMax - 1.2 * zMin);
-					var color = new THREE.Color( 0xffffff );
-					color.setHSL( 1-percent, 1, 0.5 );
-					emit( color.r, color.g, color.b, 1.0 );
-				}
-			);
-		}
-		else if (graphColorStyle == "Solid Blue")
-		{
-			// just a solid blue color			
-			graphColors.set("expr", 
-				function (emit, x, y, i, j, t) 
-				{ 
-					emit( 0.5, 0.5, 1.0, 1.0 );
-				}
-			);
-		}
-	
-	} 
-	// end of updateGraph function ==============================================================
-	
-	
-	var updateGraph = function() { updateGraphFunc(); };
+		const view = mathbox.cartesian({
+			range: [[-3, 3], [-3, 3], [-3, 3]],
+			scale: [2, 1, 2],
+		});
 
-	
-	// setting proxy:true allows interactive controls to override base position
-	var camera = mathbox.camera( { proxy: true, position: [4,2,4] } );
+		// Caracteristicas del eje X
+		view.axis({ axis: 1, width: 8, detail: 40, color: 'red' });
+		const xScale = view.scale({ axis: 1, divide: 10, nice: true, zero: true });
+		view.ticks({ width: 5, size: 15, color: 'red', zBias: 2 });
+		const xFormat = view.format({ digits: 2, font: 'Arial', weight: 'bold', style: 'normal', source: xScale });
+		view.label({ color: 'red', zIndex: 0, offset: [0, -20], points: xScale, text: xFormat });
 
-	 // save as variable to adjust later
-    view = mathbox.cartesian(
-	  {
-        range: [[xMin, xMax], [yMin, yMax], [zMin,zMax]],
-        scale: [2,1,2],
-      }
-	);
+		// Caracteristicas del eje Y
+		view.axis({ axis: 3, width: 8, detail: 40, color: 'green' });
+		const yScale = view.scale({ axis: 3, divide: 5, nice: true, zero: false });
+		view.ticks({ width: 5, size: 15, color: 'green', zBias: 2 });
+		const yFormat = view.format({ digits: 2, font: 'Arial', weight: 'bold', style: 'normal', source: yScale });
+		view.label({ color: 'green', zIndex: 0, offset: [0, 0], points: yScale, text: yFormat });
 
-	// axes
-	var xAxis = view.axis( {axis: 1, width: 8, detail: 40, color:"red"} );
-    var xScale = view.scale( {axis: 1, divide: 10, nice:true, zero:true} );
-    var xTicks = view.ticks( {width: 5, size: 15, color: "red", zBias:2} );
-    var xFormat = view.format( {digits: 2, font:"Arial", weight: "bold", style: "normal", source: xScale} );
-    var xTicksLabel = view.label( {color: "red", zIndex: 0, offset:[0,-20], points: xScale, text: xFormat} );
-	
-	var yAxis = view.axis( {axis: 3, width: 8, detail: 40, color:"green"} );
-    var yScale = view.scale( {axis: 3, divide: 5, nice:true, zero:false} );
-    var yTicks = view.ticks( {width: 5, size: 15, color: "green", zBias:2} );
-    var yFormat = view.format( {digits: 2, font:"Arial", weight: "bold", style: "normal", source: yScale} );
-    var yTicksLabel = view.label( {color: "green", zIndex: 0, offset:[0,0], points: yScale, text: yFormat} );
-	
-	var zAxis = view.axis( {axis: 2, width: 8, detail: 40, color:"blue"} );
-    var zScale = view.scale( {axis: 2, divide: 5, nice:true, zero:false} );
-    var zTicks = view.ticks( {width: 5, size: 15, color: "blue", zBias:2} );
-    var zFormat = view.format( {digits: 2, font:"Arial", weight: "bold", style: "normal", source: zScale} );
-    var zTicksLabel = view.label( {color: "blue", zIndex: 0, offset:[0,0], points: zScale, text: zFormat} );
-	
-	view.grid( {axes:[1,3], width: 2, divideX: 20, divideY: 20, opacity:0.25} );
-	
-	var graphData = view.area({
-		axes: [1,3], channels: 3, width: 64, height: 64,
-        expr: function (emit, x, y, i, j, t)
-		{
-		  var z = x*y;
-          emit( x, z, y );
-        },
-    });
-	
-	// actuall emitter set later.
-	var graphColors = view.area({
-		expr: function (emit, x,y, i,j, t) 
-		{
-			if (x < 0)
-				emit(1.0, 0.0, 0.0, 1.0);
-		    else
-				emit(0.0, 1.0, 0.0, 1.0);
-		},
-		axes: [1,3],
-		width:  64, height: 64,
-		channels: 4, // RGBA
-    });
-	
-	// create graph in two parts, because want solid and wireframe to be different colors
-	// shaded:false for a solid color (curve appearance provided by mesh)
-	// width: width of line mesh
-	// note: colors will mult. against color value, so set color to white (#FFFFFF) to let colors have complete control.
-	var graphShaded = false;	
-	var graphViewSolid = view.surface({
-		points:graphData, 
-		color:"#FFFFFF", shaded:false, fill:true, lineX:false, lineY:false, colors:graphColors, visible:true, width:0
-	});
-	
-	var graphWireVisible = true;
-	var graphViewWire = view.surface({
-		points: graphData,
-		color:"#000000", shaded:false, fill:false, lineX:true, lineY:true, visible:graphWireVisible, width:2
-    });
-	
+		// Caracteristicas del eje Z
+		view.axis({ axis: 2, width: 8, detail: 40, color: 'blue' });
+		const zScale = view.scale({ axis: 2, divide: 5, nice: true, zero: false });
+		view.ticks({ width: 5, size: 15, color: 'blue', zBias: 2 });
+		const zFormat = view.format({ digits: 2, font: 'Arial', weight: 'bold', style: 'normal', source: zScale });
+		view.label({ color: 'blue', zIndex: 0, offset: [0, 0], points: zScale, text: zFormat });
 
-	
-	updateGraphFunc();
+		view.grid({axes:[1,3], width: 2, divideX: 20, divideY: 20, opacity:0.25});
 	});
