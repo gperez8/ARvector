9a29c13b09eca48f838aa949dea3542fdc204a9d
gperez8 <gregory.facyt@gmail.com>
2017-07-04 12:05:00 -0400

    empezando con la vista de graficacion

--- dist/bundle.js
+++ dist/bundle.js
@@ -63,11 +63,12 @@
 /******/ 	__webpack_require__.p = "";
 /******/
 /******/ 	// Load entry module and return exports
-/******/ 	return __webpack_require__(__webpack_require__.s = 9);
+/******/ 	return __webpack_require__(__webpack_require__.s = 11);
 /******/ })
 /************************************************************************/
 /******/ ([
-/* 0 */
+/* 0 */,
+/* 1 */
 /***/ (function(module, exports, __webpack_require__) {
 
 "use strict";
@@ -188,7 +189,8 @@ THREEx.ArPatternFile.buildFullMarker = function (innerImageURL, onComplete) {
 };
 
 /***/ }),
-/* 1 */
+/* 2 */,
+/* 3 */
 /***/ (function(module, exports, __webpack_require__) {
 
 "use strict";
@@ -202,7 +204,7 @@ angular.module('app').controller('aboutCtrl', function ($scope) {
 });
 
 /***/ }),
-/* 2 */
+/* 4 */
 /***/ (function(module, exports, __webpack_require__) {
 
 "use strict";
@@ -227,7 +229,7 @@ angular.module('app').controller('test3', function ($scope, $location) {
 });
 
 /***/ }),
-/* 3 */
+/* 5 */
 /***/ (function(module, exports, __webpack_require__) {
 
 "use strict";
@@ -243,7 +245,7 @@ angular.module('app').controller('homeCtrl', function ($scope) {
 });
 
 /***/ }),
-/* 4 */
+/* 6 */
 /***/ (function(module, exports, __webpack_require__) {
 
 "use strict";
@@ -323,64 +325,164 @@ angular.module('app').controller('markerGenerateCtrl', function ($scope, $http) 
 });
 
 /***/ }),
-/* 5 */
+/* 7 */
 /***/ (function(module, exports, __webpack_require__) {
 
 "use strict";
 
 
 angular.module('app').controller('modelGenerateCtrl', function ($scope) {
-			$scope.hola = "hola mundo";
+	$scope.hola = "hola mundo";
 
-			var mathbox = mathBox({
-						plugins: ['core', 'controls', 'cursor', 'mathbox'],
-						controls: { klass: THREE.OrbitControls }
+	var mathbox = mathBox({
+		plugins: ['core', 'controls', 'cursor', 'mathbox'],
+		controls: { klass: THREE.OrbitControls }
+	});
+	if (mathbox.fallback) throw "WebGL not supported";
+
+	var three = mathbox.three;
+	three.renderer.setClearColor(new THREE.Color(0xFFFFFF), 1.0);
+
+	var graphData, view;
+
+	var functionText = "cos(0.5*x + 0.5*x*y)";
+
+	var pointText = "(1,1)";
+
+	var a = 1,
+	    b = 1;
+	var xMin = -3,
+	    xMax = 3,
+	    yMin = -3,
+	    yMax = 3,
+	    zMin = -3,
+	    zMax = 3;
+
+	var zAutofit = true;
+
+	// start of updateGraph function ==============================================================
+	var updateGraphFunc = function updateGraphFunc() {
+		var zFunc = Parser.parse(functionText).toJSFunction(['x', 'y']);
+		graphData.set("expr", function (emit, x, y, i, j, t) {
+			emit(x, zFunc(x, y), y);
+		});
+
+		if (zAutofit) {
+			var xStep = (xMax - xMin) / 256;
+			var yStep = (yMax - yMin) / 256;
+			var zSmallest = zFunc(xMin, yMin);
+			var zBiggest = zFunc(xMin, yMin);
+			for (var x = xMin; x <= xMax; x += xStep) {
+				for (var y = yMin; y <= yMax; y += yStep) {
+					var z = zFunc(x, y);
+					if (z < zSmallest) zSmallest = z;
+					if (z > zBiggest) zBiggest = z;
+				}
+			}
+			zMin = zSmallest;
+			zMax = zBiggest;
+		}
+		view.set("range", [[xMin, xMax], [zMin, zMax], [yMin, yMax]]);
+
+		if (graphColorStyle == "Grayscale") {
+			// zMax = white, zMin = black
+			graphColors.set("expr", function (emit, x, y, i, j, t) {
+				var z = zFunc(x, y);
+				var percent = (z - zMin) / (zMax - zMin);
+				emit(percent, percent, percent, 1.0);
 			});
-			if (mathbox.fallback) throw "WebGL not supported";
-
-			var three = mathbox.three;
-			three.renderer.setClearColor(new THREE.Color(0xFFFFFF), 1.0);
-
-			// 1. determine where the camera is looking at...
-
-			// setting proxy:true allows interactive controls to override base position
-			var camera = mathbox.camera({ proxy: true, position: [0, 0, 3] });
-
-			// 2. coordinate system that contains...
-
-			// save the view as a variable to simplify accessing it later
-			var view = mathbox.cartesian({ range: [[-6.28, 6.28], [-2, 2]], scale: [2, 1] });
-
-			// axes
-			var xAxis = view.axis({ axis: 1, width: 8, detail: 40, color: "red" });
-			var yAxis = view.axis({ axis: 2, width: 8, detail: 40, color: "green" });
-
-			// grid
-			var grid = view.grid({ width: 2, divideX: 20, divideY: 10, opacity: 0.25 });
-
-			// 3. geometric data represented via...
-
-			// the interval function will create a 1D array of data sampled between the view bounds
-			var graphData = view.interval({
-						expr: function expr(emit, x, i, t) {
-									emit(x, Math.sin(x));
-						},
-						// width is the number of data points to generate; higher numbers = higher resolution
-						width: 256,
-						// channels indicate the dimensionality of the output (set to 2 for a 2D graph)
-						channels: 2
+		} else if (graphColorStyle == "Rainbow") {
+			// rainbow hue; zMax = red, zMin = violet			
+			graphColors.set("expr", function (emit, x, y, i, j, t) {
+				var z = zFunc(x, y);
+				var percent = (z - 1.2 * zMin) / (zMax - 1.2 * zMin);
+				var color = new THREE.Color(0xffffff);
+				color.setHSL(1 - percent, 1, 0.5);
+				emit(color.r, color.g, color.b, 1.0);
 			});
+		} else if (graphColorStyle == "Solid Blue") {
+			// just a solid blue color			
+			graphColors.set("expr", function (emit, x, y, i, j, t) {
+				emit(0.5, 0.5, 1.0, 1.0);
+			});
+		}
+	};
+	// end of updateGraph function ==============================================================
 
-			// 4. choice of shape to draw it as...
 
-			var graphView = view.line({ width: 4, color: "blue" });
+	var updateGraph = function updateGraph() {
+		updateGraphFunc();
+	};
 
-			// alternatively, to view the data as points
-			// view.point( {size: 6, color: "blue"});
+	// setting proxy:true allows interactive controls to override base position
+	var camera = mathbox.camera({ proxy: true, position: [4, 2, 4] });
+
+	// save as variable to adjust later
+	view = mathbox.cartesian({
+		range: [[xMin, xMax], [yMin, yMax], [zMin, zMax]],
+		scale: [2, 1, 2]
+	});
+
+	// axes
+	var xAxis = view.axis({ axis: 1, width: 8, detail: 40, color: "red" });
+	var xScale = view.scale({ axis: 1, divide: 10, nice: true, zero: true });
+	var xTicks = view.ticks({ width: 5, size: 15, color: "red", zBias: 2 });
+	var xFormat = view.format({ digits: 2, font: "Arial", weight: "bold", style: "normal", source: xScale });
+	var xTicksLabel = view.label({ color: "red", zIndex: 0, offset: [0, -20], points: xScale, text: xFormat });
+
+	var yAxis = view.axis({ axis: 3, width: 8, detail: 40, color: "green" });
+	var yScale = view.scale({ axis: 3, divide: 5, nice: true, zero: false });
+	var yTicks = view.ticks({ width: 5, size: 15, color: "green", zBias: 2 });
+	var yFormat = view.format({ digits: 2, font: "Arial", weight: "bold", style: "normal", source: yScale });
+	var yTicksLabel = view.label({ color: "green", zIndex: 0, offset: [0, 0], points: yScale, text: yFormat });
+
+	var zAxis = view.axis({ axis: 2, width: 8, detail: 40, color: "blue" });
+	var zScale = view.scale({ axis: 2, divide: 5, nice: true, zero: false });
+	var zTicks = view.ticks({ width: 5, size: 15, color: "blue", zBias: 2 });
+	var zFormat = view.format({ digits: 2, font: "Arial", weight: "bold", style: "normal", source: zScale });
+	var zTicksLabel = view.label({ color: "blue", zIndex: 0, offset: [0, 0], points: zScale, text: zFormat });
+
+	view.grid({ axes: [1, 3], width: 2, divideX: 20, divideY: 20, opacity: 0.25 });
+
+	var graphData = view.area({
+		axes: [1, 3], channels: 3, width: 64, height: 64,
+		expr: function expr(emit, x, y, i, j, t) {
+			var z = x * y;
+			emit(x, z, y);
+		}
+	});
+
+	// actuall emitter set later.
+	var graphColors = view.area({
+		expr: function expr(emit, x, y, i, j, t) {
+			if (x < 0) emit(1.0, 0.0, 0.0, 1.0);else emit(0.0, 1.0, 0.0, 1.0);
+		},
+		axes: [1, 3],
+		width: 64, height: 64,
+		channels: 4 // RGBA
+	});
+
+	// create graph in two parts, because want solid and wireframe to be different colors
+	// shaded:false for a solid color (curve appearance provided by mesh)
+	// width: width of line mesh
+	// note: colors will mult. against color value, so set color to white (#FFFFFF) to let colors have complete control.
+	var graphShaded = false;
+	var graphViewSolid = view.surface({
+		points: graphData,
+		color: "#FFFFFF", shaded: false, fill: true, lineX: false, lineY: false, colors: graphColors, visible: true, width: 0
+	});
+
+	var graphWireVisible = true;
+	var graphViewWire = view.surface({
+		points: graphData,
+		color: "#000000", shaded: false, fill: false, lineX: true, lineY: true, visible: graphWireVisible, width: 2
+	});
+
+	updateGraphFunc();
 });
 
 /***/ }),
-/* 6 */
+/* 8 */
 /***/ (function(module, exports, __webpack_require__) {
 
 "use strict";
@@ -407,7 +509,7 @@ angular.module('app').controller('testMarkerCtrl', function ($scope) {
 });
 
 /***/ }),
-/* 7 */
+/* 9 */
 /***/ (function(module, exports, __webpack_require__) {
 
 "use strict";
@@ -416,7 +518,7 @@ angular.module('app').controller('testMarkerCtrl', function ($scope) {
 angular.module('app', ['ngRoute']);
 
 /***/ }),
-/* 8 */
+/* 10 */
 /***/ (function(module, exports, __webpack_require__) {
 
 "use strict";
@@ -449,19 +551,15 @@ angular.module('app').config(['$routeProvider', '$locationProvider', function ($
 console.log('router');
 
 /***/ }),
-/* 9 */
+/* 11 */
 /***/ (function(module, exports, __webpack_require__) {
 
 "use strict";
 
 
-__webpack_require__(7);
+__webpack_require__(9);
 
-__webpack_require__(8);
-
-__webpack_require__(1);
-
-__webpack_require__(2);
+__webpack_require__(10);
 
 __webpack_require__(3);
 
@@ -471,7 +569,11 @@ __webpack_require__(5);
 
 __webpack_require__(6);
 
-__webpack_require__(0);
+__webpack_require__(7);
+
+__webpack_require__(8);
+
+__webpack_require__(1);
 
 /***/ })
 /******/ ]);
--- public/assets/js/controllers/modelGenerateCtrl.js
+++ public/assets/js/controllers/modelGenerateCtrl.js
@@ -3,7 +3,8 @@ angular.module('app')
 	.controller('modelGenerateCtrl', ($scope) => {
 		$scope.hola = "hola mundo";
 		
-		var mathbox = mathBox({
+
+var mathbox = mathBox({
       plugins: ['core', 'controls', 'cursor', 'mathbox'],
       controls: {klass: THREE.OrbitControls}
     });
@@ -11,42 +12,165 @@ angular.module('app')
 
     var three = mathbox.three;
     three.renderer.setClearColor(new THREE.Color(0xFFFFFF), 1.0);
+
+	var graphData, view;
 	
-	// 1. determine where the camera is looking at...
+	var functionText = "cos(0.5*x + 0.5*x*y)";
+	
+	var pointText = "(1,1)";
+	
+	var a = 1, b = 1;
+	var	xMin = -3, xMax = 3, yMin = -3,	yMax = 3, zMin = -3, zMax = 3;
+	
+	var zAutofit = true;
+	
+	// start of updateGraph function ==============================================================
+	var updateGraphFunc = function() 
+	{ 
+		var zFunc = Parser.parse( functionText ).toJSFunction( ['x','y'] );
+		graphData.set("expr", 
+		    function (emit, x, y, i, j, t) 
+			{ 
+			    emit( x, zFunc(x,y), y );
+			}
+		);
+		
+		if (zAutofit)
+		{
+			var xStep = (xMax - xMin) / 256;
+			var yStep = (yMax - yMin) / 256;
+			var zSmallest = zFunc(xMin, yMin);
+			var zBiggest  = zFunc(xMin, yMin);
+			for (var x = xMin; x <= xMax; x += xStep)
+			{
+				for (var y = yMin; y <= yMax; y += yStep)
+				{
+					var z = zFunc(x,y);
+					if (z < zSmallest) zSmallest = z;
+					if (z > zBiggest)  zBiggest  = z;
+				}
+			}
+			zMin = zSmallest;
+			zMax = zBiggest;
+		}
+		view.set("range", [[xMin, xMax], [zMin,zMax], [yMin, yMax]]); 
+
+		if (graphColorStyle == "Grayscale")
+		{
+			// zMax = white, zMin = black
+			graphColors.set("expr", 
+				function (emit, x, y, i, j, t) 
+				{ 
+					var z = zFunc(x,y);
+					var percent = (z - zMin) / (zMax - zMin);
+					emit( percent, percent, percent, 1.0 );
+				}
+			);
+		}
+		else if (graphColorStyle == "Rainbow")
+		{
+			// rainbow hue; zMax = red, zMin = violet			
+			graphColors.set("expr", 
+				function (emit, x, y, i, j, t) 
+				{ 
+					var z = zFunc(x,y);
+					var percent = (z - 1.2 * zMin) / (zMax - 1.2 * zMin);
+					var color = new THREE.Color( 0xffffff );
+					color.setHSL( 1-percent, 1, 0.5 );
+					emit( color.r, color.g, color.b, 1.0 );
+				}
+			);
+		}
+		else if (graphColorStyle == "Solid Blue")
+		{
+			// just a solid blue color			
+			graphColors.set("expr", 
+				function (emit, x, y, i, j, t) 
+				{ 
+					emit( 0.5, 0.5, 1.0, 1.0 );
+				}
+			);
+		}
+	
+	} 
+	// end of updateGraph function ==============================================================
+	
+	
+	var updateGraph = function() { updateGraphFunc(); };
+
 	
 	// setting proxy:true allows interactive controls to override base position
-	var camera = mathbox.camera( { proxy: true, position: [0, 0, 3] } );
+	var camera = mathbox.camera( { proxy: true, position: [4,2,4] } );
 
-	// 2. coordinate system that contains...
-	
-	// save the view as a variable to simplify accessing it later
-    var view = mathbox.cartesian( {range: [[-6.28, 6.28], [-2, 2]], scale: [2,1]} );
+	 // save as variable to adjust later
+    view = mathbox.cartesian(
+	  {
+        range: [[xMin, xMax], [yMin, yMax], [zMin,zMax]],
+        scale: [2,1,2],
+      }
+	);
 
 	// axes
 	var xAxis = view.axis( {axis: 1, width: 8, detail: 40, color:"red"} );
-	var yAxis = view.axis( {axis: 2, width: 8, detail: 40, color:"green"} );
+    var xScale = view.scale( {axis: 1, divide: 10, nice:true, zero:true} );
+    var xTicks = view.ticks( {width: 5, size: 15, color: "red", zBias:2} );
+    var xFormat = view.format( {digits: 2, font:"Arial", weight: "bold", style: "normal", source: xScale} );
+    var xTicksLabel = view.label( {color: "red", zIndex: 0, offset:[0,-20], points: xScale, text: xFormat} );
 	
-	// grid
-	var grid = view.grid( {width: 2, divideX: 20, divideY: 10, opacity:0.25} );
+	var yAxis = view.axis( {axis: 3, width: 8, detail: 40, color:"green"} );
+    var yScale = view.scale( {axis: 3, divide: 5, nice:true, zero:false} );
+    var yTicks = view.ticks( {width: 5, size: 15, color: "green", zBias:2} );
+    var yFormat = view.format( {digits: 2, font:"Arial", weight: "bold", style: "normal", source: yScale} );
+    var yTicksLabel = view.label( {color: "green", zIndex: 0, offset:[0,0], points: yScale, text: yFormat} );
 	
-	// 3. geometric data represented via...
+	var zAxis = view.axis( {axis: 2, width: 8, detail: 40, color:"blue"} );
+    var zScale = view.scale( {axis: 2, divide: 5, nice:true, zero:false} );
+    var zTicks = view.ticks( {width: 5, size: 15, color: "blue", zBias:2} );
+    var zFormat = view.format( {digits: 2, font:"Arial", weight: "bold", style: "normal", source: zScale} );
+    var zTicksLabel = view.label( {color: "blue", zIndex: 0, offset:[0,0], points: zScale, text: zFormat} );
 	
-	// the interval function will create a 1D array of data sampled between the view bounds
-	var graphData = view.interval({
-        expr: function (emit, x, i, t)
+	view.grid( {axes:[1,3], width: 2, divideX: 20, divideY: 20, opacity:0.25} );
+	
+	var graphData = view.area({
+		axes: [1,3], channels: 3, width: 64, height: 64,
+        expr: function (emit, x, y, i, j, t)
 		{
-          emit( x, Math.sin(x) );
+		  var z = x*y;
+          emit( x, z, y );
         },
-		// width is the number of data points to generate; higher numbers = higher resolution
-        width: 256,	
-		// channels indicate the dimensionality of the output (set to 2 for a 2D graph)
-        channels: 2,
     });
 	
-	// 4. choice of shape to draw it as...
+	// actuall emitter set later.
+	var graphColors = view.area({
+		expr: function (emit, x,y, i,j, t) 
+		{
+			if (x < 0)
+				emit(1.0, 0.0, 0.0, 1.0);
+		    else
+				emit(0.0, 1.0, 0.0, 1.0);
+		},
+		axes: [1,3],
+		width:  64, height: 64,
+		channels: 4, // RGBA
+    });
 	
-    var graphView = view.line( {width: 4, color: "blue"} );
+	// create graph in two parts, because want solid and wireframe to be different colors
+	// shaded:false for a solid color (curve appearance provided by mesh)
+	// width: width of line mesh
+	// note: colors will mult. against color value, so set color to white (#FFFFFF) to let colors have complete control.
+	var graphShaded = false;	
+	var graphViewSolid = view.surface({
+		points:graphData, 
+		color:"#FFFFFF", shaded:false, fill:true, lineX:false, lineY:false, colors:graphColors, visible:true, width:0
+	});
 	
-	// alternatively, to view the data as points
-    // view.point( {size: 6, color: "blue"});
+	var graphWireVisible = true;
+	var graphViewWire = view.surface({
+		points: graphData,
+		color:"#000000", shaded:false, fill:false, lineX:true, lineY:true, visible:graphWireVisible, width:2
+    });
+	
+
+	
+	updateGraphFunc();
 	});
