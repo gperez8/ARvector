bf09c6a1ade9097b54129e2011889750df63acb6
gperez8 <gregory.facyt@gmail.com>
2017-08-13 13:19:08 -0400

    cambios en la definicion

--- public/assets/js/controllers/modelGenerateCtrl2.js
+++ public/assets/js/controllers/modelGenerateCtrl2.js
@@ -3,7 +3,7 @@
 angular.module('app')
 	.controller('modelGenerateCtrl2', ($scope, $http) => {
 		let graphMesh;
-		const segments = 10;
+		const segments = 60;
 		const xMin = -3;
 		const xMax = 3;
 		let xRange = xMax - xMin;
