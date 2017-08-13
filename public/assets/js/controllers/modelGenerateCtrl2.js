69521dfd6d7c4f7a6ec10bf21daf29fed9d946fc
gperez8 <gregory.facyt@gmail.com>
2017-08-13 12:04:25 -0400

    se cambio de grafica para pruebas

--- public/assets/js/controllers/modelGenerateCtrl2.js
+++ public/assets/js/controllers/modelGenerateCtrl2.js
@@ -2,8 +2,6 @@
 
 angular.module('app')
 	.controller('modelGenerateCtrl2', ($scope, $http) => {
-
-
 		let graphMesh;
 		const segments = 100;
 		const xMin = -3;
@@ -16,7 +14,7 @@ angular.module('app')
 		let zMax = 3;
 		let zRange = zMax - zMin;
 
-		const zFuncText = 'sin(x^2 + y^2)';
+		const zFuncText = 'x^2 - y^2';
 		let zFunc = Parser.parse(zFuncText).toJSFunction(['x', 'y']);
 
 		const scene = new THREE.Scene();
