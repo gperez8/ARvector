f0472f64cba128706ed2fbb0aa7ef07c89432ef8
gperez8 <gregory.facyt@gmail.com>
2017-08-28 07:31:54 -0400

    se corrigio login

--- public/assets/js/controllers/homeCtrl.js
+++ public/assets/js/controllers/homeCtrl.js
@@ -2,35 +2,30 @@
 
 angular.module('app')
 	.controller('homeCtrl', ($scope, $http, $auth, $location) => {
-
-		$scope.algo = 'INDEX';
-		$(document).ready(() => {
-			$('.carousel').carousel();
-		});
-
-		$scope.peticionGet = () => {
+		$scope.form;
+		/*$scope.peticionGet = () => {
 			$http.get('/resourceMarker')
 				.then((data) => {
 					console.log('res->', data.data.res);
 				});
+		};*/
+		$scope.login = () => {
+			
+			console.log("scope.form", $scope.form);
+
+			$auth.login($scope.form)
+				.then((data) => {
+					console.log('data', data);
+					$location.path('/createMarker');
+				})
+				.catch((response) => {
+					console.log('log incorrecto');
+				});
 		};
-		$scope.peticionPost = () => {
-			$auth.login({
-				email: 'gregory.facyt@gmail.com',
-				password: 'skdjsfldkjdfl',
-			})
-			.then((data) => {
-				console.log('data', data);
-				$location.path('/createMarker');
-			})
-			.catch((response) => {
-				console.log('log incorrecto');
-			});
-		};
-		$scope.peticionPut = () => {
+		/*$scope.peticionPut = () => {
 			$http.patch('/resourceMarker');
 		};
 		$scope.peticionDelete = () => {
 			$http.delete('/resourceMarker');
-		};
+		};*/
 	});
